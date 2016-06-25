14f0945a87cbf646d3450edad73037e7631ddc21
Damien Kerbart <damien.kerbart@nusseo.com>
2016-06-25 10:06:33 +0200

    Plugins

--- www/js/services/appService.js
+++ www/js/services/appService.js
@@ -1,8 +1,8 @@
 starter.service('appService', function ($log, $http, $localStorage, $q, $ionicLoading, $ionicHistory) {
 	return {
 		checpointAPIUrl : function() {
-			// return "http://ns328613.ip-37-187-114.eu:8080/checkpoint/api/";
-			 return "http://192.168.1.41:8080/checkpoint/api/";
+			return "http://ns328613.ip-37-187-114.eu:8080/checkpoint/api/";
+			// return "http://192.168.1.41:8080/checkpoint/api/";
 		},
 		isUserConnected : function() {
 			return $localStorage.user != undefined;
